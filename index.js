33837adf63686dcbb67522c673c3bbc9d495f40f
Andrew Szucs <andrew0szucs@gmail.com>
2017-02-12 14:24:37 +0100

    feat(express): Initialize express

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict'
 
 const config = require('./server/config')
+const app = require('./server')
 
-console.log(config.serverUrl ? 'success' : 'failure')
+// process.env.PORT lets the port to be set by Heroku
+const PORT = process.env.PORT
+app.set('port', PORT)
+
+const server = app.listen(app.get('port'), () =>
+  console.log(`Express app is listening at \n${config.serverUrl}`))
--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
 'use strict'
 
 // Setup and export the Express app
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/login', (req, res) => res.send('Login'))
+// app.post('/login', (req, res) => {
+
+// })
+
+module.exports = app
