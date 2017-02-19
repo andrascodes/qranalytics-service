ba944269f1a50cefeed25871df31f78ea7be1299
Andrew Szucs <andrew0szucs@gmail.com>
2017-02-19 22:40:05 +0100

    feat(dbResync): add reSync options based on env vars

--- server/config/index.js
+++ server/config/index.js
@@ -7,16 +7,25 @@ const config = {
 }
 
 // Check the environment
-const env = process.env.NODE_ENV || 'development';
-
+const env = process.env.NODE_ENV || 'development'
 // Set Database connection URL
 if(env === 'production') {
-    config.databaseUrl = process.env.DATABASE_URL;
-    config.serverUrl = `${process.env.HEROKU_SERVER_URL}:${process.env.PORT}/`;
+    config.databaseUrl = process.env.DATABASE_URL
+    config.serverUrl = `${process.env.HEROKU_SERVER_URL}:${process.env.PORT}/`
 }
 else {
-    config.databaseUrl = process.env.LOCAL_DATABASE_URL;
-    config.serverUrl = `${process.env.LOCAL_SERVER_URL}:${process.env.PORT}/`;
+    config.databaseUrl = process.env.LOCAL_DATABASE_URL
+    config.serverUrl = `${process.env.LOCAL_SERVER_URL}:${process.env.PORT}/`
+}
+
+const resyncDb = process.env.FORCE_DB
+if(resyncDb.toUpperCase() === 'TRUE') {
+    console.log('DB set to resync.')
+    config.dbOptions = { force: true }
+}
+else {
+    console.log('DB is not being resynced.')
+    config.dbOptions = {}
 }
 
 module.exports = config;
